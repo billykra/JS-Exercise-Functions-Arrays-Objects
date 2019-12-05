b8618d7eee169255d40c0a4e87be2cc5252a4521
Bill Kramer <billykra@gmail.com>
2019-12-04 17:50:00 -0800

    Q7

--- index.js
+++ index.js
@@ -220,9 +220,17 @@ function sortCarInventory(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  let modelYears = [];
+  for (let i = 0; i < inventory.length; i++){
+    modelYears.push(inventory[i].car_year)
+  }
+  return modelYears;
 }
+
+
+
+
 
 /**
  * ### Challenge `getOlderCars`
@@ -240,6 +248,10 @@ function getOlderCars(/* code here */) {
   /* code here */
 }
 
+
+
+
+
 /**
  * ### Challenge `getGermanCars`
  * 
@@ -254,6 +266,10 @@ function getOlderCars(/* code here */) {
 function getGermanCars(/* code here */) {
   /* code here */
 }
+
+
+
+
 
 /**
  * ### Challenge refactor to arrow functions
@@ -276,6 +292,16 @@ function getGermanCars(/* code here */) {
 const sum = null; // code here!
 const addFive = null; // code here!
 const argTimesTwo = null; // code here!
+
+
+
+
+
+
+
+
+
+
 
 /**
  * ### Challenge `carMaker`
